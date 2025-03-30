04f3f17a8126803a85cb780ccfff0bccf014c0c6
Aniket Yadav <yadavaniket7611@gmail.com>
2025-03-30 14:13:07 +0530

    Search feature added

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,42 @@ const App = () => {
 const [characters, setCharacters] = useState<Character[]>([])
 const [error, setError] = useState<string | null>(null);
 const [searchParams, setSearchParams] = useState<string>("")
+const [loading, setLoading] = useState<boolean>(false);
 console.log(searchParams);
 
 useEffect(() => {
+  if(searchParams.trim() === ""){
+    setCharacters([]);
+    setError(null);
+    return
+  }
     const getCharacters = async ()=>{
-     
-      const data = await fetchCharacters(searchParams);
-      if(data.length === 0){
-        setError("NO CHARACTERS FOUND")
-        setCharacters([]);
+      setLoading(true);
+      try {
+        const data = await fetchCharacters(searchParams);
+        if(data.length === 0){
+          setError("NO CHARACTERS FOUND");
+          setCharacters([])
+        }
+        else{
+          setCharacters(data);
+          setError(null)
+        }
+      } catch (error) {
+        setError("Failed to fetch data")
+        setCharacters([])
+        console.log(error);
+        
       }
-      setCharacters(data)
+      finally{
+        setLoading(false)
+      }
     }
     const timer = setTimeout(() => {
       getCharacters();
+      
     },2000)
+    
     return () => clearTimeout(timer)
     
   }, [searchParams])
@@ -29,20 +50,21 @@ useEffect(() => {
   return (
     <div className="h-[100vh] bg-black text-white">
       <h1>Anime Characters</h1>
-      <input className="border-4 border-white" type="text" value={searchParams} onChange={(e) => setSearchParams(e.target.value)}/>
+      <input 
+      className="border-4 border-white" 
+      type="text" value={searchParams} 
+      onChange={(e) => setSearchParams(e.target.value)}/>
+      {loading && <p>Searching........</p>}
       {error && <p>{error}</p>}
-      {characters.length > 0 ? (
+
+      {!loading && characters.length > 0 && (
         characters.map((char) => (
           <div key={char.mal_id}>
             <h1>{char.name}</h1>
             <img src={char.images.jpg.image_url} alt="" />
           </div>
         ))
-      ) :
-      (
-        <p>Loading.....</p>
-      )
-    }
+      ) }
 
     </div>
   )
